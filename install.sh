d91a5ed422127171caff3588ac421aa18b6bbda3
Majorx500 <majorx500@gmail.com>
2026-03-22 13:12:07 +0100

    papermc start + main install script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+OPTION=""
+GAME=""
+GAME_DIR=""
+GAME_VER=""
+
+
+
+whiptail --title "Menu example" --menu "Choose an option" 25 78 16 \
+  
+  > OPTION
+
+echo "$OPTION"
+
+case $OPTION in
+  ""
+esac
+
+whiptail --title "Choose Game Server" --backtitle "Major's Game Server Manager" --radiolist \
+  "Choose user's permissions" 20 78 4 \
+  "NET_OUTBOUND" "Allow connections to other hosts" ON \
+  "NET_INBOUND" "Allow connections from other hosts" OFF \
+  "LOCAL_MOUNT" "Allow mounting of local devices" OFF \
+  "REMOTE_MOUNT" "Allow mounting of remote devices" OFF
---
+++ scripts/install/papermc.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# $1 -- gameversion $2 -- gamedir
+
+GAME_URL=$(curl -X 'GET' \
+  "https://fill.papermc.io/v3/projects/paper/versions/$1/builds/latest" \
+  -H 'accept: application/json' | jq -r '.downloads."server:default".url')
+
+mkdir "$2/serverfiles"
+
+cd "$2" || exit
+
+wget "$GAME_URL" -O "$2/serverfiles/papermc.jar" 1>/dev/null
+chmod +x "$2/serverfiles/papermc.jar"
+
+if (whiptail --title "Eula Agreement" --yesno "Do you agree to the Minecraft EULA? (https://aka.ms/MinecraftEULA)" 8 70); then
+  echo "eula=true" >$2/serverfiles/eula.txt
+else
+  echo "Eula needed to run server. Edit $2/serverfiles/eula.txt to agree to it."
+  echo "eula=false" >$2/serverfiles/eula.txt
+fi
+
+echo "Installing server"
+
+echo "stop" | java -jar "./serverfiles/papermc.jar" --nogui 1>/dev/null 2>/dev/null
+
+echo "Server created"
